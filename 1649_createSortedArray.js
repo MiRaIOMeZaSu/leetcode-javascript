88dfe42e35bd09152573aa94d29494ebe7068fa5
miraiomezasu <3792927266@qq.com>
2021-06-07 14:56:01 +0800

    1649:done

--- 1649_createSortedArray.js
+++ 1649_createSortedArray.js
@@ -2,75 +2,70 @@
  * @param {number[]} instructions
  * @return {number}
  */
-var createSortedArray = function (instructions) {
-    // 每次都是二分查找(贪心)
-    if (instructions.length <= 2) {
+var Tree = function (maxNum) {
+    var treeSize = maxNum & (maxNum - 1) == 0 ? 2 * maxNum : 4 * maxNum;
+    this.treeArr = new Array(treeSize).fill(0);
+};
+
+Tree.prototype.getRangeCount = function (targetStart, targetEnd, currStart, currEnd, n = 0) {
+    if (targetEnd < currStart || targetStart > currEnd) {
         return 0;
     }
-    var ret = 0;
-    var arr = [instructions[0]];
-    if (instructions[1] > instructions[0]) {
-        arr.push(instructions[1]);
-    } else {
-        arr.unshift(instructions[1]);
+    if (targetStart <= currStart && targetEnd >= currEnd) {
+        return this.treeArr[n];
     }
-    for (var i = 2; i < instructions.length; i++) {
-        // 此时开始二分
-        var left = 0;
-        var right = arr.length - 1;
-        var mid = left + Math.floor((right - left) / 2);
-        while (left + 1 < right) {
-            mid = left + Math.floor((right - left) / 2);
-            if (instructions[i] > arr[mid]) {
-                left = mid;
-            } else if (instructions[i] < arr[mid]) {
-                right = mid;
-            } else {
-                // 此时相等
-                var temp = mid;
-                while (temp >= 0 && arr[temp] == instructions[i]) {
-                    temp--;
-                }
-                var l = temp + 1;
-                temp = mid;
-                while (temp <= arr.length - 1 && arr[temp] == instructions[i]) {
-                    temp++;
-                }
-                var index;
-                if (l < arr.length - temp) {
-                    index = l;
-                } else {
-                    index = temp;
-                }
-                var toPlus = 0;
-                if (index == arr.length || index == 0) {
-                    toPlus = 0;
-                } else {
-                    toPlus = Math.min(index, arr.length - index);
-                }
-                ret += toPlus;
-                arr.splice(index, 0, instructions[i]);
-                break;
-            }
-        }
-        if (arr.length <= i) {
-            var index;
-            if (instructions[i] <= arr[left]) {
-                index = left;
-            } else if (instructions[i] >= arr[right]) {
-                index = right + 1;
-            } else {
-                index = right;
-            }
-            arr.splice(index, 0, instructions[i]);
-            ret += Math.min(index, arr.length - 1 - index);
-        }
+    // 开始二分
+    var temp = (currStart + currEnd) >> 1;
+    var a = this.getRangeCount(targetStart, targetEnd, currStart, temp, n * 2 + 1);
+    var b = this.getRangeCount(targetStart, targetEnd, temp + 1, currEnd, n * 2 + 2);
+    return a + b;
+}
 
+Tree.prototype.addNum = function (num, currStart, currEnd, n = 0) {
+    // 延迟更新?
+    if (currStart <= num && currEnd >= num) {
+        this.treeArr[n]++;
+        if (currStart != currEnd) {
+            // 二分赋值
+            var temp = (currStart + currEnd) >> 1;
+            this.addNum(num, currStart, temp, n * 2 + 1);
+            this.addNum(num, temp + 1, currEnd, n * 2 + 2);
+        }
     }
-    if (ret > 1000000007) {
-        ret = ret % 1000000007;
+}
+
+var createSortedArray = function (instructions) {
+    // 每次都是二分查找(贪心)
+    // 直接使用搜索树无法获取相对数量!
+    // 使用线段树判断?
+    // 使用两个栈
+    // 输入数字存在相同!(统计数量?)
+    // 使用线段树统计!
+    size = instructions.length;
+    if (size < 3) {
+        return 0;
+    }
+    var maxNum = Number.MIN_VALUE;
+    for (i = 0; i < size; i++) {
+        maxNum = Math.max(instructions[i], maxNum);
+    }
+    var tree = new Tree(maxNum);
+    tree.addNum(instructions[0], 0, maxNum)
+    tree.addNum(instructions[1], 0, maxNum)
+    var ret = 0;
+    var pivot = 1000000007;
+    for (i = 2; i < size; i++) {
+        // 包含前面的
+        var temp1 = tree.getRangeCount(0, instructions[i] - 1, 0, maxNum);
+        temp1 = Math.min(temp1, i - temp1);
+        var temp2 = tree.getRangeCount(instructions[i] + 1, maxNum, 0, maxNum);
+        temp2 = Math.min(temp2, i - temp2);
+        var cost = Math.min(temp1, temp2);
+        ret += cost;
+        ret = ret % pivot;
+        tree.addNum(instructions[i], 0, maxNum);
     }
     return ret;
 };
-var ret = createSortedArray([3, 2, 1, 3, 2, 1, 3, 2, 1]);
+var ret = createSortedArray([1, 3, 3, 3, 2, 4, 2, 1, 2]);
 console.log(ret);
